4e592c6b0d8a18410c72d2538318db03d12e1d92
Tomo Hashimoto <tomo@squairone.com>
2019-02-20 16:39:44 +0900

    update fix all

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var dat = {
+    title: 'SSL接続サンプル',
+    serverProtocol: global.serverProtocol,
+    
+    serverPort: global.serverPort,
+  }
+  res.render('index', { dat: dat });
 });
 
 module.exports = router;
